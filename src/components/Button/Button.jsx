89f7c238a148f536b6d4a36606e0e05e541b384a
Nick Lemmon <nick.lemmon@gmail.com>
2018-07-14 11:40:18 -0400

    Add button component with eslint config

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import classnames from 'classnames';
+import { NavLink } from 'react-router-dom';
+import './Button.css';
+
+export default class Button extends React.Component {
+  render() {
+    const {
+      type,
+      classes,
+      linkTo,
+      content,
+      fullWidth,
+      role,
+      tabIndex
+    } = this.props;
+    const calculatedClassNames = `Button Button--${ type } ${ classnames( { classes } ) }`;
+    const styles = { width: fullWidth ? '100%' : '' };
+
+    return (
+      <React.Fragment>
+        { linkTo ?
+          <NavLink
+            className={ calculatedClassNames } 
+            style={ styles }
+            to={ linkTo }
+            role={ role }
+            tabIndex={ tabIndex }
+          >
+            { content }
+          </NavLink>
+        :
+          <button
+            className={ calculatedClassNames } 
+            style={ styles }
+            onClick={ this.props.onClick }
+            role={ role }
+            tabIndex={ tabIndex }
+          >
+            { content }
+          </button>
+        }
+      </React.Fragment>
+    )
+  }
+}
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
 import { Welcome } from '@storybook/react/demo';
+import '../index.css';
+import Button from '../components/Button/Button';
 
-import Button from '../components/Button';
+storiesOf( 'Welcome', module ).add( 'to Storybook', () => <Welcome showApp={ linkTo( 'Button' ) } /> );
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button/>)
+storiesOf( 'Button', module )
+  .add( 'with text', () => <Button content='Click Me!'/> )
+  .add( 'with primary type', () => <Button type='primary' content='Primary!' /> )
+  .add( 'with secondary type', () => <Button type='secondary' content='Secondary!' /> )
+  .add( 'with full width style', () => <Button fullWidth={ true } content='Full width!' /> )
+  .add( 'with link', () => <Button linkTo='/' content='With Link' /> )
