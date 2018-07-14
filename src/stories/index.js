15f09f4f8bec6843262dd4c0347d3eaa6d708e09
Nick Lemmon <nick.lemmon@gmail.com>
2018-07-14 14:25:59 -0400

    Add Heading component

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,7 +4,9 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
 import '../index.css';
+
 import Button from '../components/Button/Button';
+import Heading from '../components/Heading/Heading';
 
 storiesOf( 'Welcome', module ).add( 'to Storybook', () => <Welcome showApp={ linkTo( 'Button' ) } /> );
 
@@ -14,3 +16,13 @@ storiesOf( 'Button', module )
   .add( 'with secondary type', () => <Button type='secondary' content='Secondary!' /> )
   .add( 'with full width style', () => <Button fullWidth={ true } content='Full width!' /> )
   .add( 'with link', () => <Button linkTo='/' content='With Link' /> )
+
+storiesOf( 'Heading', module )
+  .add( 'with level 1', () => <Heading level='1' content='Heading level 1' /> )
+  .add( 'with level 2', () => <Heading level='2' content='Heading level 2' /> )
+  .add( 'with level 3', () => <Heading level='3' content='Heading level 3' /> )
+  .add( 'with level 4', () => <Heading level='4' content='Heading level 4' /> )
+  .add( 'with level 5', () => <Heading level='5' content='Heading level 5' /> )
+  .add( 'with level 6', () => <Heading level='6' content='Heading level 6' /> )
+
+
