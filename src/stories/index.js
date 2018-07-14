a54eedc2cfdf98bcee03e8b639b167fadaadbdcf
Nick Lemmon <nick.lemmon@gmail.com>
2018-07-14 14:47:11 -0400

    Add Alert component

--- src/stories/index.js
+++ src/stories/index.js
@@ -8,6 +8,7 @@ import { Router } from 'react-router';
 import '../index.css';
 import Button from '../components/Button/Button';
 import Heading from '../components/Heading/Heading';
+import Alert from '../components/Alert/Alert';
 
 storiesOf( 'Welcome', module ).add( 'to Storybook', () => <Welcome showApp={ linkTo( 'Button' ) } /> );
 
@@ -22,7 +23,7 @@ storiesOf( 'Button', module )
         <Button linkTo={ undefined } content='With Link' />
       </Router>
     )
-  })
+  } )
 
 storiesOf( 'Heading', module )
   .add( 'with level 1', () => <Heading level='1' content='Heading level 1' /> )
@@ -31,5 +32,11 @@ storiesOf( 'Heading', module )
   .add( 'with level 4', () => <Heading level='4' content='Heading level 4' /> )
   .add( 'with level 5', () => <Heading level='5' content='Heading level 5' /> )
   .add( 'with level 6', () => <Heading level='6' content='Heading level 6' /> )
+
+storiesOf( 'Alert', module )
+  .add( 'with type error', () => <Alert type='error' content='This is an error alert.' /> )
+  .add( 'with type success', () => <Alert type='success' content='This is an success alert.' /> )
+  .add( 'with type attention', () => <Alert type='attention' content='This is an attention alert.' /> )
+  .add( 'with type info', () => <Alert type='info' content='This is an info alert.' /> )
 
 
