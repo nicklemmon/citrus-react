c7c61531ea727af4c8dfcde2eae48de9d37872af
Nick Lemmon <nick.lemmon@gmail.com>
2020-08-05 16:05:55 -0400

    master - Moved styled components outside of function

--- src/components/ButtonWrapper/ButtonWrapper.js
+++ src/components/ButtonWrapper/ButtonWrapper.js
@@ -2,22 +2,22 @@ import React, { cloneElement, isValidElement } from 'react'
 import styled from 'styled-components'
 import { space } from 'styled-system'
 
-export default function ButtonWrapper(props) {
-  const ButtonWrapper = styled.div`
-    ${space}
-    width: 100%;
-    margin-top: ${props => props.theme.space[3]};
+const StyledWrapper = styled.div`
+  ${space}
+  width: 100%;
+  margin-top: ${props => props.theme.space[3]};
 
-    > * + * {
-      margin-top: ${props => props.theme.space[2]};
+  > * + * {
+    margin-top: ${props => props.theme.space[2]};
 
-      @media (min-width: ${props => props.theme.breakpoints[0]}) {
-        margin-top: 0;
-        margin-left: ${props => props.theme.space[2]};
-      }
+    @media (min-width: ${props => props.theme.breakpoints[0]}) {
+      margin-top: 0;
+      margin-left: ${props => props.theme.space[2]};
     }
-  `
+  }
+`
 
+export default function ButtonWrapper(props) {
   const renderChildren = () => {
     return React.Children.map(props.children, (child, index) => {
       if (!isValidElement(child)) return
@@ -32,7 +32,7 @@ export default function ButtonWrapper(props) {
     })
   }
 
-  return <ButtonWrapper>{renderChildren()}</ButtonWrapper>
+  return <StyledWrapper>{renderChildren()}</StyledWrapper>
 }
 
 ButtonWrapper.propTypes = {
