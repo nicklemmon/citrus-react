b38beafb6568809e38d7d41b939caaa1137fefbf
Nick Lemmon <nick.lemmon@gmail.com>
2020-08-15 20:43:45 -0400

    Import InlineAlert

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button'
 export { default as ButtonWrapper } from './ButtonWrapper'
 export { default as CitrusTheme } from './CitrusTheme'
+export { default as InlineAlert } from './InlineAlert'
